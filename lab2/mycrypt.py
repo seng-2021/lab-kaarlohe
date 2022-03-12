c47326989b5656639102a46730a7cc5da4be4ac0
Kaarlo Heikkilä <kaaheikk20@student.oulu.fi>
2022-03-12 19:13:47 +0200

    Implemented decode() and modified encode() to pass original tests

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -5,20 +5,35 @@ def encode(s):
         raise TypeError
     origlen = len(s)
     crypted = ""
+    dump = ""
     digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
     if len(s) > 1000:
         raise ValueError
-    for c in s:
-        if c.isalpha():
-            if c.islower():
+    elif len(s) < 1:
+        raise ValueError
+    if len(s.strip()) < len(s):
+        raise ValueError
+    tempStr = s.ljust(1000)
+    i = 0
+    for i in range(1000):
+        if tempStr[i].isalpha():
+            c = tempStr[i]
+            # If character is å,ä or ö raises ValueError
+            if ord(c) > 122:
+                raise ValueError
+            elif c.islower():
                 c=c.upper()
             # Rot13 the character for maximum security
             crypted+=codecs.encode(c,'rot13')
-        elif c in digitmapping:
-          crypted+=digitmapping[c]
-
+        elif tempStr[i] in digitmapping:
+            crypted+=digitmapping[tempStr[i]]
+        # Rot13 empty space for security against time attacks
+        elif tempStr[i].isspace():
+            dump+=codecs.encode("a",'rot13')
+        else:
+            raise ValueError
+            
     return crypted
-
+    
 def decode(s):
-    return s
-
+    return encode(s).lower()
